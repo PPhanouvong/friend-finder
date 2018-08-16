390ab610e1a93c2e146e968b5073758bb58b21f1
pphanouvong <pphanouvong1@gmail.com>
2018-08-16 19:36:46 -0400

    fixed the img error

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,43 +2,43 @@
 var operatorArray = [
     {
         name: 'Thermite',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiZma61pvDcAhWNTN8KHVIfCEEQjRx6BAgBEAU&url=http%3A%2F%2Ftomclancy.wikia.com%2Fwiki%2FFile%3AThermite.png&psig=AOvVaw3RLhPHpn4DsgCqF1qfUiR-&ust=1534465080139628",
+        photo: "https://vignette.wikia.nocookie.net/tomclancy/images/1/1c/Thermite.png/revision/latest/scale-to-width-down/446?cb=20150917064433",
         score: [3, 1, 3, 1, 3, 4, 5, 5, 1, 1]
     }, {
         name: 'Blitz',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwislY3WpvDcAhUlWN8KHeCbA8MQjRx6BAgBEAU&url=http%3A%2F%2Frainbowsix.wikia.com%2Fwiki%2FFile%3ABlitz_Portrait.png&psig=AOvVaw3RLhPHpn4DsgCqF1qfUiR-&ust=1534465080139628",
+        photo: "https://vignette.wikia.nocookie.net/rainbowsix/images/6/6c/Large-blitz.2e74d4b8.png/revision/latest?cb=20171223234918",
         score: [5, 3, 1, 5, 3, 5, 1, 1, 5, 1]
     }, {
         name: 'Ash',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwifpYXfpfDcAhVBdt8KHVBqBSkQjRx6BAgBEAU&url=https%3A%2F%2Frainbowsixsiege.gamepedia.com%2FAsh&psig=AOvVaw2xRCu1vHdcVIoQmXHNoSSj&ust=1534464663221235",
+        photo: "https://d1u5p3l4wpay3k.cloudfront.net/rainbowsixsiege_gamepedia/thumb/b/b1/Ash.png/550px-Ash.png?version=03999340a81fbf4a39004ba80d0c3095",
         score: [5, 1, 3, 1, 3, 5, 5, 5, 1, 1]
     }, {
         name: 'Hibana',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi328q-pvDcAhXkQd8KHRejA_cQjRx6BAgBEAU&url=http%3A%2F%2Frainbowsix.wikia.com%2Fwiki%2FFile%3AHibana_-_Full_Body.png&psig=AOvVaw3RLhPHpn4DsgCqF1qfUiR-&ust=1534465080139628",
+        photo: "https://vignette.wikia.nocookie.net/rainbowsix/images/3/37/Hibana_-_Full_Body.png/revision/latest/scale-to-width-down/198?cb=20180110070841",
         score: [5, 3, 1, 1, 4, 5, 5, 5, 1, 2]
     }, {
         name: 'Lion',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiBrL7OpfDcAhUIWN8KHXqaCe4QjRx6BAgBEAU&url=http%3A%2F%2Frainbowsix.wikia.com%2Fwiki%2FLion&psig=AOvVaw2xRCu1vHdcVIoQmXHNoSSj&ust=1534464663221235",
+        photo: "https://vignette.wikia.nocookie.net/rainbowsix/images/4/4b/Lion_-_Full_Body.png/revision/latest/scale-to-width-down/620?cb=20180224044503",
         score: [2, 5, 3, 1, 5, 2, 4, 4, 1, 3]
     }, {
         name: 'Dokkaebi',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjKxdCGpvDcAhUDON8KHQeUBRsQjRx6BAgBEAU&url=https%3A%2F%2Fstore.ubi.com%2Fie%2Ftom-clancy-s-rainbow-six-siege%2F56c494ad88a7e300458b4d5a.html&psig=AOvVaw3RLhPHpn4DsgCqF1qfUiR-&ust=1534465080139628",
+        photo: "https://ubistatic19-a.akamaihd.net/resource/en-us/game/rainbow6/siege/r6-operator-dokkaebi_307132.png",
         score: [1, 5, 2, 1, 5, 2, 5, 4, 2, 2]
     }, {
         name: 'IQ',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiZwp-OpvDcAhXCl-AKHRVlDIMQjRx6BAgBEAU&url=https%3A%2F%2Frainbowsixsiege.gamepedia.com%2FIQ&psig=AOvVaw3RLhPHpn4DsgCqF1qfUiR-&ust=1534465080139628",
+        photo: "https://d1u5p3l4wpay3k.cloudfront.net/rainbowsixsiege_gamepedia/thumb/6/68/IQ.png/550px-IQ.png?version=ff788a6d05888d6795796ab8abb22bee",
         score: [3, 3, 3, 1, 5, 3, 4, 4, 2, 3]
     }, {
         name: 'Twitch',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiEv9TNpvDcAhVOUt8KHQZUDbUQjRx6BAgBEAU&url=http%3A%2F%2Frainbowsix.wikia.com%2Fwiki%2FTwitch%2FGallery&psig=AOvVaw3RLhPHpn4DsgCqF1qfUiR-&ust=1534465080139628",
+        photo: "https://ubistatic19-a.akamaihd.net/resource/en-us/game/rainbow6/siege/r6-operator-twitch_229845.png",
         score: [1, 4, 5, 1, 5, 3, 4, 3, 1, 4]
     }, {
         name: 'Glaz',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiQssPYpfDcAhUvTd8KHckXAwEQjRx6BAgBEAU&url=http%3A%2F%2Fpowerairsoft.com.br%2Fhome%2Fr6-operators-glaz_229118%2F&psig=AOvVaw2xRCu1vHdcVIoQmXHNoSSj&ust=1534464663221235",
+        photo: "http://powerairsoft.com.br/wp-content/uploads/2016/01/R6-operators-glaz_229118.png",
         score: [1, 5, 1, 1, 5, 2, 5, 5, 1, 5]
     }, {
         name: 'Fuze',
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiq3JPwpfDcAhUiTt8KHSdHADEQjRx6BAgBEAU&url=https%3A%2F%2Fru.kisspng.com%2Fpng-tom-clancys-rainbow-six-siege-tom-clancys-the-divi-1443239%2F&psig=AOvVaw2xRCu1vHdcVIoQmXHNoSSj&ust=1534464663221235",
+        photo: "https://banner2.kisspng.com/20180203/jsq/kisspng-tom-clancys-rainbow-six-siege-tom-clancys-the-divi-tom-clancys-rainbow-six-transparent-background-5a7566619db5d0.056178791517643361646.jpg",
         score: [5, 5, 5, 5, 3, 4, 4, 1, 1, 1]
     }
 ];
